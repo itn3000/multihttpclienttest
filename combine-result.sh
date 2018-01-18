30bc6a1d45843b5929d74ec3e4fa497b6f72e205
itn3000 <itn3000@gmail.com>
2018-01-19 02:18:37 +0900

    add combine script

---
+++ combine-result.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat <(echo fw,method,loop,concurrent,limit,time) \
+	<(sed -e 's/.*net,elapsed/net,/' mthc-net-result-win.txt) \
+	<(sed -e 's/.*core,elapsed/core,/' mthc-core-result-win.txt) \
+	> mthc-result-win.csv
+cat <(echo fw,method,loop,concurrent,limit,time) \
+	<(sed -e 's/.*net,elapsed/net,/' mthc-net-result-linux.txt) \
+	<(sed -e 's/.*core,elapsed/core,/' mthc-core-result-linux.txt) \
+	> mthc-result-linux.csv
